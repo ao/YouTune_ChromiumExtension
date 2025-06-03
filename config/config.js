23a11144e11e475e3b307f7e86583ca0866a9d66
Andrew Odendaal <aoaws@amazon.com>
2025-06-03 14:00:03 +0400

    Add .gitignore, update extension description, and correct review URL

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 // Chrome configuration
 const support_url = "https://ataiva.com/youtune-chromium-extension-help";
-const review_url = "https://chromewebstore.google.com/detail/youtune/zttqzeqtqzkcqiqzjigmlqnhlntennnz/reviews"; /* Placeholder until extension published */
+const review_url = "https://chromewebstore.google.com/detail/youtune/jdkpldbjebmobimbadioblaopknlbokn/reviews";
 
 export {support_url, review_url};
